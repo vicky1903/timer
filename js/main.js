b4d19899424f0a0c678e041a56c4d1949ee28688
vicky1903 <50952787+vicky1903@users.noreply.github.com>
2019-06-10 13:59:27 +0200

    calcule des du temps restant en js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ var dateEvenement = new Date("jul 27 00:00:00 2100");
 
 //on calcule la differences des deux dates puis on divise par 1000 pour les avoirs en seconde//
 var totalSecondes = (dateEvenement - dateActuelle) / 1000;
+
+//calculer le nombre de jours restants. Le nombre étant en secondes, 1 jour = 60 x 60 x 24 secondes.//
+var jours = Math.floor(totalSecondes / (60 * 60 * 24));
+// calculer le nombre d heures restant.  1 heure = 60 x 60 secondes//
+var heures = Math.floor((totalSecondes - (jours * 60 * 60 * 24)) / (60 * 60));
+//meme techniques pour les minutes et les secondes//
+minutes = Math.floor((totalSecondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+secondes = Math.floor(totalSecondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
